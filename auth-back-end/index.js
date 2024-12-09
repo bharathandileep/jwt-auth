613a22ca0df4f4f12189f2c12b17304d160a1e22
bharathan01 <bharathandileep01@gmail.com>
2024-12-09 12:54:05 +0530

    new brach

--- auth-back-end/index.js
+++ auth-back-end/index.js
@@ -26,11 +26,6 @@ app.get("/", (req, res) => {
 });
 
 
-// added new changes in the auth
-// new git rules are added to the main branch
-// in emp branches
-// new rules been addedd
-
 // invalid api URL
 app.use("**", (req, res) => {
   res.status(BAD_REQUEST).json({
