cf31c0d6165dc71d06e4814d6365639aa6ac306a
bharathan01 <bharathandileep01@gmail.com>
2024-12-09 11:54:19 +0530

    changes are made

--- auth-back-end/index.js
+++ auth-back-end/index.js
@@ -28,6 +28,10 @@ app.get("/", (req, res) => {
 
 // added new changes in the auth
 // changes are created
+// there is new chages are craeated 
+
+
+
 // invalid api URL
 app.use("**", (req, res) => {
   res.status(BAD_REQUEST).json({
